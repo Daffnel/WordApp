ee189ee841f37dcb8926e96870836e41f67f104a
Zeldisen <jeanette.norberg82@gmail.com>
2025-03-25 09:36:21 +0100

    added loggo and button, and flags

--- WordApp/ViewController.swift
+++ WordApp/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var swedenFlag: UILabel!
+   
+    @IBOutlet weak var englishFlag: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Loggo image
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "WordApp-loggo")
+        imageView.frame = CGRect(x: 100, y: 30, width: 200, height: 200)
+        imageView.contentMode = .scaleAspectFit
+        
+        view.addSubview(imageView)
+        
+        swedenFlag.text = " 🇸🇪 "
+        englishFlag.text = " 🇬🇧 "
+       
+        
+        
     }
 
-
+   
+  
+    
 }
 
